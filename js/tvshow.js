f3a2f997afa7c36164c3328043be6f66efdc4913
rizkiokta <mrizkioktafianto@gmail.com>
2021-06-27 19:25:54 +0700

    security debug

--- js/tvshow.js
+++ js/tvshow.js
@@ -2,7 +2,7 @@ const form = document.querySelector('#searchForm');
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
     const searchTerm = form.elements.query.value;
-    const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${searchTerm}`);
+    const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
     console.log(res);
     displayImage(res.data);
 });
